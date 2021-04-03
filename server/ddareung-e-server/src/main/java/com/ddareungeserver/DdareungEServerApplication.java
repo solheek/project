a0246d80e232270005ee524a64fd9cf5934d35c9
solheek <sh1025b@gmail.com>
2021-04-03 13:25:15 +0900

    feat:New server project

---
+++ server/ddareung-e-server/src/main/java/com/ddareungeserver/DdareungEServerApplication.java
@@ -0,0 +1,13 @@
+package com.ddareungeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdareungEServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdareungEServerApplication.class, args);
+	}
+
+}
---
+++ server/ddareung-e-server/src/test/java/com/ddareungeserver/DdareungEServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddareungeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdareungEServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
